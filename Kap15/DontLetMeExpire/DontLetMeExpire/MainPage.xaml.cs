2009b29630a43dcc8286268d71bfc132fe1cdc67
André Krämer <andre@andrekraemer.de>
2024-06-19 17:15:15 +0200

    Kap 15 DontLetMeExpireStart

---
+++ Kap15/DontLetMeExpire/DontLetMeExpire/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using DontLetMeExpire.ViewModels;
+
+namespace DontLetMeExpire;
+
+public partial class MainPage : ContentPage
+{
+  private readonly MainViewModel _viewModel;
+
+  public MainPage(MainViewModel viewModel)
+  {
+    InitializeComponent();
+    BindingContext = _viewModel = viewModel;
+  }
+
+  protected override async void OnNavigatedTo(NavigatedToEventArgs args)
+  {
+    await _viewModel.InitializeAsync();
+    base.OnNavigatedTo(args);
+  }
+}
