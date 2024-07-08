febfb90ef36852d9b95c1a246f736aa567ad695c
André Krämer <andre@andrekraemer.de>
2024-07-08 16:44:03 +0200

    Kap 19 WIP

---
+++ Kap19/WebserviceSample/WebserviceSample/ViewModels/NewSpeakerViewModel.cs
@@ -0,0 +1,73 @@
+﻿using MvvmSample.ViewModels;
+using WebserviceSample.Models;
+using WebserviceSample.Services;
+
+namespace WebserviceSample.ViewModels;
+
+public class NewSpeakerViewModel : ViewModelBase
+{
+  private string _name;
+  private string _company;
+  private string _bio;
+  private readonly ISpeakerService _speakerService;
+
+  public NewSpeakerViewModel(ISpeakerService speakerService)
+    {
+    SaveCommand = new Command(OnSave, ValidateSave);
+    CancelCommand = new Command(OnCancel);
+    PropertyChanged +=
+        (_, __) => SaveCommand.ChangeCanExecute();
+    _speakerService = speakerService;
+  }
+
+  private bool ValidateSave()
+  {
+    return !string.IsNullOrWhiteSpace(_name)
+        && !string.IsNullOrWhiteSpace(_company)
+        && !string.IsNullOrWhiteSpace(_bio);
+  }
+
+  public string Name
+  {
+    get => _name;
+    set => SetProperty(ref _name, value);
+  }
+
+  public string Company
+  {
+    get => _company;
+    set => SetProperty(ref _company, value);
+  }
+
+
+  public string Bio
+  {
+    get => _bio;
+    set => SetProperty(ref _bio, value);
+  }
+
+
+  public Command SaveCommand { get; }
+  public Command CancelCommand { get; }
+
+  private async void OnCancel()
+  {
+    // This will pop the current page off the navigation stack
+    await Shell.Current.GoToAsync("..");
+  }
+
+  private async void OnSave()
+  {
+    Speaker newSpeaker = new Speaker()
+    {
+      FullName = Name,
+      Company = Company,
+      Bio = Bio
+    };
+
+    await _speakerService.CreateAsync(newSpeaker);
+
+    // This will pop the current page off the navigation stack
+    await Shell.Current.GoToAsync("..");
+  }
+}
---
+++ Kap19/WebserviceSample/WebserviceSample/ViewModels/SpeakersViewModel.cs
@@ -0,0 +1,97 @@
+﻿using MvvmSample.ViewModels;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using WebserviceSample.Models;
+using WebserviceSample.Services;
+using WebserviceSample.Views;
+
+
+namespace WebserviceSample.ViewModels
+{
+  public class SpeakersViewModel : ViewModelBase
+  {
+    private Speaker? _selectedSpeaker;
+    private readonly ISpeakerService _speakerService;
+
+    public ObservableCollection<Speaker> Speakers { get; }
+    public Command LoadSpeakersCommand { get; }
+    public Command AddSpeakerCommand { get; }
+
+    public Command DeleteSpeakerCommand { get; }
+    public Command<Speaker> SpeakerTapped { get; }
+
+    public SpeakersViewModel(ISpeakerService speakerService)
+    {
+      Title = "Liste der Konferenzsprecher";
+      Speakers = new ObservableCollection<Speaker>();
+      LoadSpeakersCommand = new Command(async () => await ExecuteLoadItemsCommand());
+
+      SpeakerTapped = new Command<Speaker>(OnItemSelected);
+
+      AddSpeakerCommand = new Command(OnAddItem);
+
+      DeleteSpeakerCommand = new Command(OnDelete);
+      _speakerService = speakerService;
+    }
+
+    private async void OnDelete(object speaker)
+    {
+      if (speaker is Speaker speakerToDelete)
+      {
+        await _speakerService.DeleteAsync(speakerToDelete.Id);
+      }
+    }
+
+    async Task ExecuteLoadItemsCommand()
+    {
+      IsBusy = true;
+
+      try
+      {
+        Speakers.Clear();
+        var items = await _speakerService.GetAsync(true);
+        foreach (var item in items)
+        {
+          Speakers.Add(item);
+        }
+      }
+      catch (Exception ex)
+      {
+        Debug.WriteLine(ex);
+      }
+      finally
+      {
+        IsBusy = false;
+      }
+    }
+
+    public void Initialize()
+    {
+      IsBusy = true;
+      SelectedSpeaker = null;
+    }
+
+    public Speaker SelectedSpeaker
+    {
+      get => _selectedSpeaker;
+      set
+      {
+        SetProperty(ref _selectedSpeaker, value);
+        OnItemSelected(value);
+      }
+    }
+
+    private async void OnAddItem(object obj)
+    {
+      await Shell.Current.GoToAsync(nameof(NewSpeakerPage));
+    }
+
+    async void OnItemSelected(Speaker speaker)
+    {
+      if (speaker == null)
+        return;
+
+      await Shell.Current.GoToAsync($"{nameof(SpeakerDetailPage)}?{nameof(SpeakerDetailViewModel.SpeakerId)}={speaker.Id}");
+    }
+  }
+}
