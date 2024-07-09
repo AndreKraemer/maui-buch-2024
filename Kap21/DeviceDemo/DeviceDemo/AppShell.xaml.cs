7a01baf9e3d496acc7404b9244de32148214f498
André Krämer <andre@andrekraemer.de>
2024-07-09 23:55:47 +0200

    Kap21 Wip

---
+++ Kap21/DeviceDemo/DeviceDemo/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace DeviceDemo
+{
+  public partial class AppShell : Shell
+  {
+    public AppShell()
+    {
+      InitializeComponent();
+    }
+  }
+}
---
+++ Kap21/DeviceDemo/DeviceDemo/MauiProgram.cs
@@ -0,0 +1,33 @@
+﻿using DeviceDemo.ViewModels;
+using DeviceDemo.Views;
+using Microsoft.Extensions.Logging;
+
+namespace DeviceDemo
+{
+  public static class MauiProgram
+  {
+    public static MauiApp CreateMauiApp()
+    {
+      var builder = MauiApp.CreateBuilder();
+      builder
+        .UseMauiApp<App>()
+        .ConfigureFonts(fonts =>
+        {
+          fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+          fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+        });
+
+#if DEBUG
+  		builder.Logging.AddDebug();
+#endif
+      builder.Services.AddTransient<AppInfoViewModel>();
+      builder.Services.AddTransient<AppInfoPage>();
+      builder.Services.AddSingleton<IAppInfo>(AppInfo.Current);
+
+      builder.Services.AddTransient<MediaViewModel>();
+      builder.Services.AddTransient<MediaPage>();
+      builder.Services.AddSingleton<IMediaPicker>(MediaPicker.Default);
+      return builder.Build();
+    }
+  }
+}
