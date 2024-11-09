23e4e13cfd84e3912e053aabca8b189512dacc05
André Krämer <andre.kraemer@qualitybytes.de>
2024-11-09 19:28:41 +0100

    MainPage.xaml angepasst

--- Kap08/LayoutSample/LayoutSample/App.xaml.cs
+++ Kap08/LayoutSample/LayoutSample/App.xaml.cs
@@ -8,4 +8,15 @@ public partial class App : Application
 
 		MainPage = new AppShell();
 	}
+
+#if WINDOWS
+  protected override Window CreateWindow(IActivationState activationState)
+  {
+    Window window = base.CreateWindow(activationState);
+
+    window.Width = 500;
+
+    return window;
+  }
+#endif
 }
